00162268ce4e0e2691b8f599f5c2854ff150f9d7
Will <doggy.huang@gmail.com>
2015-09-05 15:08:46 +0800

    16 請依序執行 mytask2, mytask1, default 工作

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['mytask1'], function() {
+	console.log('My Default Task');
+});
+
+gulp.task('mytask1', ['mytask2'], function() {
+	console.log('Task1 Output');
+});
+
+gulp.task('mytask2', function() {
+	console.log('Task2 Output');
+});
